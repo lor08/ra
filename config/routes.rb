ac0251b8098f74e0fcb59a8d5a8e409960d1e279
lor08 <spike@ya.ru>
2016-04-01 01:01:12 +0300

    add route and dinamic title

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,14 @@
 Rails.application.routes.draw do
   get 'static_pages/about'
 
-  get 'static_pages/index'
-
-  get 'static_pages/gps'
-
-  get 'static_pages/graphics'
-
-  get 'static_pages/rinex'
-
-  get 'static_pages/tables'
-
-  get 'static_pages/weather'
-
-  get 'static_pages/ztd'
-
-  get 'static_pages/tec'
+  #get 'static_pages/index'
+  match '/',  to: 'static_pages#index', via: 'get'
+  match '/gps',  to: 'static_pages#gps', via: 'get'
+  match '/graphics',  to: 'static_pages#graphics', via: 'get'
+  match '/tables',  to: 'static_pages#tables', via: 'get'
+  match '/weather',  to: 'static_pages#weather', via: 'get'
+  match '/ztd',  to: 'static_pages#ztd', via: 'get'
+  match '/tec',  to: 'static_pages#tec', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
